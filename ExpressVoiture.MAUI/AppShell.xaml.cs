d0627e5e47fbaf0837fb9cc57ef3f92bf0320196
Renato SICLER <renatosicler33@gmail.com>
2025-09-25 15:52:59 +0200

    Creation du projet MAUI et suppression des dossier plus utiliser de MVC

---
+++ ExpressVoiture.MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpressVoiture.MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ExpressVoiture.MAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ExpressVoiture.MAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
